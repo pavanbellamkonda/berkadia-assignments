7c6a58795121f61d6c5e7d5090b1f7758643d96e
unknown <pavanbellamkonda98@gmail.com>
2018-12-01 06:08:06 +0530

    gg

---
+++ week 3/node/CRUD with express/test.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+app.get('/retrieve', function (req, res) {
+    let data = fs.readFileSync('data.json', 'utf8');
+    let json = JSON.parse(data);
+    console.log(JSON.stringify(json, null, 4))
+    res.end(JSON.stringify(json, null, 4))
+ })
+
+ app.put('/create', function(req, res) {
+    collectRequestData(req, result => {
+        let pres = JSON.parse(result)
+        let data = fs.readFileSync('data.json', 'utf8');
+        let json = JSON.parse(data);
+        for(var key in pres){
+            if (key in json)
+                res.end("User(s) already exists!")
+            else
+                json[key] = pres[key]
+        }
+        fs.writeFileSync('data.json', JSON.stringify(json, null, 4), 'utf8')
+        console.log(JSON.stringify(json, null, 4))
+        res.end(JSON.stringify(json, null, 4))
+ });
+});
+
+app.post('/update', function(req, res){
+    collectRequestData(req, result => {
+        let pres = JSON.parse(result)
+        let data = fs.readFileSync('data.json', 'utf8');
+        let json = JSON.parse(data);
+        for(var key in pres){
+            if (key in json)
+                json[key] = pres[key]
+            else
+                res.end("User(s) does not exist!")
+        }
+        fs.writeFileSync('data.json', JSON.stringify(json, null, 4), 'utf8')
+        console.log(JSON.stringify(json, null, 4))
+        res.end(JSON.stringify(json, null, 4))
+    });
+});
+
+app.delete('/delete', function(req, res){
+    collectRequestData(req, result => {
+        let pres = JSON.parse(result)
+        let data = fs.readFileSync('data.json', 'utf8');
+        let json = JSON.parse(data);
+        for(var key in pres){
+            if (key in json)
+                delete json[key]
+            else
+                res.end("User(s) does not exist!")
+        }
+        fs.writeFileSync('data.json', JSON.stringify(json, null, 4), 'utf8')
+        console.log(JSON.stringify(json, null, 4))
+        res.end(JSON.stringify(json, null, 4))
+    });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at port: %s", port)
+})
+
+function collectRequestData(request, callback) {
+    let body = '';
+    request.on('data', chunk => {
+        body += chunk.toString();
+    });
+    request.on('end', () => {
+        callback(body);
+    });
+}
---
+++ week 3/node/CRUD without express/server.js
@@ -0,0 +1,82 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    var url = req.url;
+    if(url === '/'){
+        var html = fs.readFileSync('index.html', 'utf8');
+        res.end(html)
+    }
+    else if(url === '/retrieve'){
+        let data = fs.readFileSync('data.json', 'utf8');
+        let json = JSON.parse(data);
+        console.log(JSON.stringify(json, null, 4))
+        res.end(JSON.stringify(json, null, 4))
+    }
+    else if(url === '/create'){
+        collectRequestData(req, result => {
+            let pres = JSON.parse(result)
+            let data = fs.readFileSync('data.json', 'utf8');
+            let json = JSON.parse(data);
+            for(var key in pres){
+                if (key in json)
+                    res.end("User(s) already exists!")
+                else
+                    json[key] = pres[key]
+            }
+            fs.writeFileSync('data.json', JSON.stringify(json, null, 4), 'utf8')
+            console.log(JSON.stringify(json, null, 4))
+            res.end(JSON.stringify(json, null, 4))
+        });
+    }
+    else if(url === '/update'){
+        collectRequestData(req, result => {
+            let pres = JSON.parse(result)
+            let data = fs.readFileSync('data.json', 'utf8');
+            let json = JSON.parse(data);
+            for(var key in pres){
+                if (key in json)
+                    json[key] = pres[key]
+                else
+                    res.end("User(s) does not exist!")
+            }
+            fs.writeFileSync('data.json', JSON.stringify(json, null, 4), 'utf8')
+            console.log(JSON.stringify(json, null, 4))
+            res.end(JSON.stringify(json, null, 4))
+        });
+    }
+    else if(url === '/delete'){
+        collectRequestData(req, result => {
+            let pres = JSON.parse(result)
+            let data = fs.readFileSync('data.json', 'utf8');
+            let json = JSON.parse(data);
+            for(var key in pres){
+                if (key in json)
+                    delete json[key]
+                else
+                    res.end("User(s) does not exist!")
+            }
+            fs.writeFileSync('data.json', JSON.stringify(json, null, 4), 'utf8')
+            console.log(JSON.stringify(json, null, 4))
+            res.end(JSON.stringify(json, null, 4))
+        });
+    }
+    else{
+        res.end("<p>Wrong page! Go <a href=" + `"http://localhost:3000/"` +">Home!</a></p>")
+    }
+    
+
+});
+
+function collectRequestData(request, callback) {
+    let body = '';
+    request.on('data', chunk => {
+        body += chunk.toString();
+    });
+    request.on('end', () => {
+        callback(body);
+    });
+}
+
+console.log("Server listening at port 3000.")
+server.listen(3000)
