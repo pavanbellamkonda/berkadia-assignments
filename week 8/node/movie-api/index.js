49513b94de1a2c0c52c10d3a4390b293f82ff661
unknown <pavanbellamkonda98@gmail.com>
2019-01-01 10:49:29 +0530

    movie api

--- week 8/node/movie-api/index.js
+++ week 8/node/movie-api/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const BodyParser = require('body-parser');
-app.use(BodyParser.json({limit:"10mb"}));
+app.use(BodyParser.json({ limit: "10mb" }));
 
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -13,14 +13,49 @@ var allowCrossDomain = function(req, res, next) {
 
 app.use(allowCrossDomain);
 
+//lists all movies available
 app.get('/movies', (req, res) => {
     var data = JSON.parse(fs.readFileSync('data.json'))
     res.end(JSON.stringify(data["movies"], null, 4))
 })
 
+//lists all theatres of the pincode or all the theatres
 app.get('/theatres', (req, res) => {
-    var data = JSON.parse(fs.readFileSync('data.json'))
-    res.end(JSON.stringify(data["theatres"], null, 4))
+    var pin = req.query.pin;
+    var movie = req.query.movie;
+    if (pin && movie) {
+        var theatres = []
+        var data = JSON.parse(fs.readFileSync('data.json'))
+        var byPin = data['theatres'].filter(theatre => theatre.pin === parseInt(req.query.pin));
+        var byMovie = data['shows'].filter(show => show.movie === req.query.movie)
+        byPin.forEach(item => {
+            if (byMovie.filter(x => item.name === x.theatre).length) {
+                theatres.push(item)
+            }
+        })
+        res.end(JSON.stringify(theatres, null, 4))
+    }
+    else if (pin) {
+        var data = JSON.parse(fs.readFileSync('data.json'))
+        var theatres = data['theatres'].filter(theatre => theatre.pin === parseInt(req.query.pin));
+        res.end(JSON.stringify(theatres, null, 4))
+    }
+    else if (movie) {
+        var theatres = []
+        var data = JSON.parse(fs.readFileSync('data.json'))
+        var byMovie = data['shows'].filter(show => show.movie === req.query.movie)
+        data['theatres'].forEach(item => {
+            if (byMovie.filter(x => item.name === x.theatre).length) {
+                theatres.push(item)
+            }
+        })
+        res.end(JSON.stringify(theatres, null, 4))
+    }
+    else {
+        var data = JSON.parse(fs.readFileSync('data.json'))
+        res.end(JSON.stringify(data['theatres'], null, 4))
+
+    }
 })
 
 app.get('/movie-info', (req, res) => {
@@ -47,8 +82,6 @@ app.get('/theatre-shows', (req, res) => {
     res.end(JSON.stringify(data['shows'].filter(show => show.theatre === theatre), null, 4));
 })
 
-
-
-app.listen(3000, '192.168.0.101', () => {
+app.listen(3000, () => {
     console.log('Listening at 3000')
 })
