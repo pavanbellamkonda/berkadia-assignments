99d8155a8742844016ed467f11f41312e574e23c
unknown <pavanbellamkonda98@gmail.com>
2019-01-01 22:37:48 +0530

    mm

--- week 8/node/movie-api/index.js
+++ week 8/node/movie-api/index.js
@@ -4,22 +4,11 @@ const fs = require('fs')
 const BodyParser = require('body-parser');
 app.use(BodyParser.json({ limit: "10mb" }));
 
-var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-
-app.use(allowCrossDomain);
-
-//lists all movies available
 app.get('/movies', (req, res) => {
     var data = JSON.parse(fs.readFileSync('data.json'))
     res.end(JSON.stringify(data["movies"], null, 4))
 })
 
-//lists all theatres of the pincode or all the theatres
 app.get('/theatres', (req, res) => {
     var pin = req.query.pin;
     var movie = req.query.movie;
@@ -54,32 +43,57 @@ app.get('/theatres', (req, res) => {
     else {
         var data = JSON.parse(fs.readFileSync('data.json'))
         res.end(JSON.stringify(data['theatres'], null, 4))
-
     }
 })
 
-app.get('/movie-info', (req, res) => {
-    var name = req.query.name;
-    var data = JSON.parse(fs.readFileSync('data.json'));
-    res.end(JSON.stringify(data['movies'].find(movie => movie.name === name), null, 4));
-})
-
-app.get('/theatre-info', (req, res) => {
-    var name = req.query.name;
-    var data = JSON.parse(fs.readFileSync('data.json'));
-    res.end(JSON.stringify(data['theatres'].find(theatre => theatre.name === name), null, 4));
-})
-
-app.get('/movie-shows', (req, res) => {
-    var movie = req.query.movie;
-    var data = JSON.parse(fs.readFileSync('data.json'));
-    res.end(JSON.stringify(data['shows'].filter(show => show.movie === movie), null, 4));
-})
-
-app.get('/theatre-shows', (req, res) => {
+app.get('/shows', (req, res) => {
     var theatre = req.query.theatre;
-    var data = JSON.parse(fs.readFileSync('data.json'));
-    res.end(JSON.stringify(data['shows'].filter(show => show.theatre === theatre), null, 4));
+    var movie = req.query.movie;
+    if(theatre && movie){
+        var data = JSON.parse(fs.readFileSync('data.json'));
+        var shows = data['shows'].filter(show => show.theatre === theatre && show.movie === movie);
+        res.end(JSON.stringify(shows, null, 4))
+    }
+    else if(theatre){
+        var data = JSON.parse(fs.readFileSync('data.json'));
+        var shows = data['shows'].filter(show => show.theatre === theatre);
+        res.end(JSON.stringify(shows, null, 4))
+    }
+    else if(movie){
+        var data = JSON.parse(fs.readFileSync('data.json'));
+        var shows = data['shows'].filter(show => show.movie === movie);
+        res.end(JSON.stringify(shows, null, 4))
+    }
+    else {
+        var data = JSON.parse(fs.readFileSync('data.json'))
+        res.end(JSON.stringify(data['shows'], null, 4))
+    }
+})
+
+app.post('/seats', (req, res) => {
+    var data = JSON.parse(fs.readFileSync('data.json'))
+    var body = req.body;
+    var movie = body.movie;
+    var theatre = body.theatre;
+    var date = body.date;
+    var time = body.time;
+    var show = data['shows'].find(show => show.theatre === theatre && show.movie === movie)
+    var seats = show['seats'].find(seat => seat.date === date)
+    var result = seats['timings'].find(r => r.time === time)
+    res.end(JSON.stringify(result['tickets'], null, 4))
+})
+
+app.post('/book', (req, res) => {
+    var data = JSON.parse(fs.readFileSync('data.json'))
+    var body = req.body;
+    var movie = body.movie;
+    var theatre = body.theatre;
+    var date = body.date;
+    var time = body.time;
+    var show = data['shows'].find(show => show.theatre === theatre && show.movie === movie)
+    var seats = show['seats'].find(seat => seat.date === date)
+    var result = seats['timings'].find(r => r.time === time)
+    res.end(JSON.stringify(result['tickets'], null, 4))
 })
 
 app.listen(3000, () => {
