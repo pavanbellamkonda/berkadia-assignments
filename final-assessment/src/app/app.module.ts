ba261460ee8ab67ae9efbdfd78792d1bafabc88b
Pavan Bellamkonda <31280326+pa1b@users.noreply.github.com>
2019-01-19 14:34:07 +0530

    Add files via upload

--- final-assessment/src/app/app.module.ts
+++ final-assessment/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { QuestionsComponent } from './questions/questions.component';
 import { QlistService } from './qlist.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    QuestionsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
